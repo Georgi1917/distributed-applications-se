d76dbae5b11d9a9a7340662e99eb3e057687cd2b
Gosho Zvqr <mamarida15@abv.bg>
2026-05-16 18:05:34 +0300

    added AddedOn and UpdatedOn Fields

--- course-work/implementations/JobListing/src/main/java/com/example/JobListing/Entities/BaseEntity.java
+++ course-work/implementations/JobListing/src/main/java/com/example/JobListing/Entities/BaseEntity.java
@@ -1,20 +1,32 @@
 package com.example.JobListing.Entities;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity
 {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
+
+    @CreatedDate
+    @Column(name="added_on", nullable = false, updatable = false)
+    private LocalDateTime AddedOn;
+
+    @LastModifiedDate
+    @Column(name="updated_on")
+    private LocalDateTime UpdatedOn;
 
 }
